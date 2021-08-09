a1140ee9b79b1db3dd1182919c98f6b9d3f9a875
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-08-09 11:08:57 -0300

    Merge manual rama desarro-nacho con desarrollo-franco

--- src/main/java/danms/sueldos/rest/ReciboSueldoRest.java
+++ src/main/java/danms/sueldos/rest/ReciboSueldoRest.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import danms.sueldos.domain.CodigoDetalle;
+import danms.sueldos.domain.DetalleRecibo;
 import danms.sueldos.domain.ReciboSueldo;
 import danms.sueldos.services.interfaces.ReciboSueldoService;
 import io.swagger.annotations.ApiOperation;
@@ -26,21 +27,20 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/sueldos/recibosueldo")
 public class ReciboSueldoRest {
-	
+
 	@Autowired
 	ReciboSueldoService reciboSueldoService;
-	
+
 	/*------------Codigo detalle-------------------*/
-	
-	@PostMapping(path="/codigodetalle")
+
+	@PostMapping(path = "/codigodetalle")
 	@ApiOperation(value = "Permite crear un codigo detalle")
-	@ApiResponses(value = { 
-			@ApiResponse(code = 200, message = "Guardado correctamente"),
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Guardado correctamente"),
 			@ApiResponse(code = 400, message = "No se pudo guardar") })
 	public ResponseEntity<CodigoDetalle> crearCodigoDetalle(@RequestBody CodigoDetalle codigoDetalleN) {
 		return ResponseEntity.of(reciboSueldoService.guardarCodigoDetalle(codigoDetalleN));
 	}
-	
+
 	@GetMapping(path = "/codigodetalle/{codigoDetalle}")
 	@ApiOperation(value = "Permite obtener un codigo detalle dada su codigo detalle. Aclaracion: codigoDetalle distinto a idDetalle")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "El codigo detalle se encontro"),
@@ -48,13 +48,13 @@ public class ReciboSueldoRest {
 	public ResponseEntity<CodigoDetalle> getCodigoDetalle(@PathVariable Integer codigoDetalle) {
 		return ResponseEntity.of(reciboSueldoService.getCodigoDetalle(codigoDetalle));
 	}
-	
-	@GetMapping(path="/codigodetalle")
+
+	@GetMapping(path = "/codigodetalle")
 	@ApiOperation(value = "Permite obtener todos los codigos detalle")
 	public ResponseEntity<List<CodigoDetalle>> getAllCodigoDetalle() {
 		return ResponseEntity.ok(reciboSueldoService.getAllCodigoDetalle());
 	}
-	
+
 	@DeleteMapping(path = "/codigodetalle/{codigoDetalle}")
 	@ApiOperation(value = "Borrar un codigo detalle. El codigo detalle se debe pasar en el path")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Borrado correctamente"),
@@ -63,7 +63,8 @@ public class ReciboSueldoRest {
 	public ResponseEntity<CodigoDetalle> borrarSucursal(@PathVariable Integer codigoDetalle) {
 		Optional<CodigoDetalle> optCodigoDetalleABorrar = this.reciboSueldoService.getCodigoDetalle(codigoDetalle);
 		if (optCodigoDetalleABorrar.isPresent()) {
-			Optional<CodigoDetalle> optCodigoDetalleBorrada = this.reciboSueldoService.borrarCodigoDetalle(optCodigoDetalleABorrar.get());
+			Optional<CodigoDetalle> optCodigoDetalleBorrada = this.reciboSueldoService
+					.borrarCodigoDetalle(optCodigoDetalleABorrar.get());
 			if (optCodigoDetalleBorrada.isPresent()) {
 				// Se borro correctamente
 				return ResponseEntity.of(optCodigoDetalleBorrada);
@@ -76,28 +77,29 @@ public class ReciboSueldoRest {
 			return ResponseEntity.badRequest().build();
 		}
 	}
-	
+
 	@PutMapping(path = "/codigodetalle/{id}")
 	@ApiOperation(value = "Permite actualizar un codigo detalle. El id del codigo detalle debe estar en el path")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Actualizado correctamente"),
 			@ApiResponse(code = 400, message = "No se pudo actualizar") })
-	public ResponseEntity<CodigoDetalle> actualizarCodigoDetalle(@RequestBody CodigoDetalle codigoDetalle, @PathVariable Integer id) {
+	public ResponseEntity<CodigoDetalle> actualizarCodigoDetalle(@RequestBody CodigoDetalle codigoDetalle,
+			@PathVariable Integer id) {
 		// Chequeamos que la sucursal exista
-		Optional<CodigoDetalle> optCodigoDetalleAActualizar = this.reciboSueldoService.getCodigoDetalle(codigoDetalle.getCodigoDetalle());
+		Optional<CodigoDetalle> optCodigoDetalleAActualizar = this.reciboSueldoService
+				.getCodigoDetalle(codigoDetalle.getCodigoDetalle());
 		if (optCodigoDetalleAActualizar.isPresent()) {
-			//La sucursal existe
+			// La sucursal existe
 			codigoDetalle.setId(id);
 			return ResponseEntity.of(reciboSueldoService.actualizarCodigoDetalle(codigoDetalle));
-		} 
-		else {
-			//La sucursal no existe
+		} else {
+			// La sucursal no existe
 			return ResponseEntity.badRequest().build();
 		}
 	}
-	
+
 	/*------------Recibo sueldo-------------------*/
-	//TODO: quizas sea necesario un get recibo de sueldo por id de empleado!
-	
+	// TODO: quizas sea necesario un get recibo de sueldo por id de empleado!
+
 	@GetMapping(path = "/{id}")
 	@ApiOperation(value = "Permite obtener un recibo de sueldo dada su id.")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "El recibo de sueldo se encontro"),
@@ -105,33 +107,70 @@ public class ReciboSueldoRest {
 	public ResponseEntity<ReciboSueldo> getReciboSueldo(@PathVariable Integer id) {
 		return ResponseEntity.of(reciboSueldoService.getReciboSueldo(id));
 	}
-	
+
 	@GetMapping
 	@ApiOperation(value = "Permite obtener todos los recibo de sueldo")
 	public ResponseEntity<List<ReciboSueldo>> getAllReciboDeSueldo() {
 		return ResponseEntity.ok(reciboSueldoService.getAllReciboSueldo());
 	}
-	
+
 	@PutMapping(path = "/{id}")
 	@ApiOperation(value = "Permite actualizar un recibo de sueldo. El id del recibo debe estar en el path")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Actualizado correctamente"),
 			@ApiResponse(code = 400, message = "No se pudo actualizar") })
-	public ResponseEntity<ReciboSueldo> actualizarReciboSueldo(@RequestBody ReciboSueldo reciboSueldo, @PathVariable Integer id) {
+	public ResponseEntity<ReciboSueldo> actualizarReciboSueldo(@RequestBody ReciboSueldo reciboSueldo,
+			@PathVariable Integer id) {
 		// Chequeamos que la sucursal exista
 		Optional<ReciboSueldo> optReciboSueldoAActualizar = this.reciboSueldoService.getReciboSueldo(id);
 		if (optReciboSueldoAActualizar.isPresent()) {
-			//El recibo sueldo existe
+			// El recibo sueldo existe
 			reciboSueldo.setId(id);
 			return ResponseEntity.of(reciboSueldoService.actualizarReciboSueldo(reciboSueldo));
-		} 
-		else {
-			//El recibo sueldo no existe
+		} else {
+			// El recibo sueldo no existe
 			return ResponseEntity.badRequest().build();
 		}
 	}
-	
-	
-	
-	
-	
+	/*------------Detalle Recibo-------------------*/
+
+	@GetMapping(path = "/detalleRecibo/{idDetalle}")
+	@ApiOperation(value = "Permite obtener un detalleRecibo dado su id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "El detalle recibo se encontro"),
+			@ApiResponse(code = 405, message = "El id recibida es nula") })
+
+	// TODO: fijarse el codigo de retorno
+	public ResponseEntity<DetalleRecibo> getDetalleRecibo(@PathVariable Integer idDetalle) {
+
+		if (idDetalle == null) {
+			return ResponseEntity.badRequest().build();
+		}
+
+		return ResponseEntity.of(reciboSueldoService.getDetalleRecibo(idDetalle));
+	}
+
+	@GetMapping(path = "/detalleRecibo")
+	@ApiOperation(value = "Permite obtener todos los detalleRecibo")
+	@ApiResponses(value = @ApiResponse(code = 200, message = "Se encontraron detalles recibo"))
+	public ResponseEntity<List<DetalleRecibo>> getAllDetalleRecibo() {
+		return ResponseEntity.ok(reciboSueldoService.getAllDetalleRecibo());
+	}
+
+	@PutMapping(path = "/detalleRecibo/{idDetalle}")
+	@ApiOperation(value = "Permite actualizar un detalleRecibo. El id del detalle recibo debe estar en el path")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Actualizado correctamente"),
+			@ApiResponse(code = 400, message = "No se pudo actualizar") })
+	public ResponseEntity<DetalleRecibo> actualizarCodigoDetalle(@RequestBody DetalleRecibo detalleRecibo,
+			@PathVariable Integer idDetalle) {
+		// Chequeamos que la sucursal exista
+		Optional<DetalleRecibo> optDetalleAActualizar = this.reciboSueldoService.getDetalleRecibo(idDetalle);
+		if (optDetalleAActualizar.isPresent()) {
+			// El detalle existe
+			detalleRecibo.setId(idDetalle);
+			return ResponseEntity.of(reciboSueldoService.actualizarDetalleRecibo(detalleRecibo));
+		} else {
+			// El detalle no existe
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
 }
--- src/test/java/danms/sueldos/rest/ReciboSueldoRestTest.java
+++ src/test/java/danms/sueldos/rest/ReciboSueldoRestTest.java
@@ -41,6 +41,8 @@ class ReciboSueldoRestTest {
 
 	private String ENDPOINT_RECIBO_SUELDO = "/api/sueldos/recibosueldo";
 
+	private String ENDPOINT_DETALLE_SUELDO = "/api/sueldos/recibosueldo/detalleRecibo";
+	
 	private static final Logger logger = LoggerFactory.getLogger(SucursalRestTest.class);
 
 	@Autowired
@@ -63,7 +65,7 @@ class ReciboSueldoRestTest {
 	
 	@Autowired
 	DetalleReciboRepository detalleRepo;
-
+	
 	@LocalServerPort
 	String puerto;
 
@@ -426,6 +428,177 @@ class ReciboSueldoRestTest {
 		assertEquals(rs.getNumeroRecibo(), optReciboSueldoActualizado.get().getNumeroRecibo());
 		assertEquals(rs.getLugarDePago(), optReciboSueldoActualizado.get().getLugarDePago());
 		logger.info("[RestAPI]Fin test: actualizarReciboSueldo");
+	}
+	
+	/*-----------------------------Detalle recibo--------------------------------------*/
+	
+	@Test
+	void getDetalleRecibo() {
+		// codigoDetalle 1
+		CodigoDetalle cd1 = new CodigoDetalle();
+		cd1.setCodigoDetalle(100);
+		cd1.setDescripcion("La primer descripcion");
+
+		// ---------------------------------------------------
+
+		cd1 = codigoDetalleRepo.saveAndFlush(cd1);
+
+		// detalleRecibo 1
+		DetalleRecibo dr1 = new DetalleRecibo();
+		dr1.setCodigoDetalle(cd1);
+		dr1.setHaber(1500.00);
+		dr1.setDeduccion(null);
+		dr1.setPorcentaje(10.5);
+
+		detalleRepo.saveAndFlush(dr1);
+
+		logger.info("[RestAPI]Inicio test: getDetalleSueldo");
+		String server = "http://localhost:" + puerto + ENDPOINT_DETALLE_SUELDO;
+
+		// Re-configuramos la ruta del path
+		server += "/" + dr1.getId();
+
+		// Detalle Recibo
+		ResponseEntity<DetalleRecibo> respuesta = testRestTemplate.exchange(server, HttpMethod.GET, null,
+				DetalleRecibo.class);
+		assertTrue(respuesta.getStatusCode().equals(HttpStatus.OK));
+
+		assertEquals(dr1.getId(), respuesta.getBody().getId());
+		assertEquals(dr1.getDeduccion(), respuesta.getBody().getDeduccion());
+		assertEquals(dr1.getHaber(), respuesta.getBody().getHaber());
+		assertEquals(dr1.getPorcentaje(), respuesta.getBody().getPorcentaje());
+		assertEquals(dr1.getCodigoDetalle().getCodigoDetalle(),
+				respuesta.getBody().getCodigoDetalle().getCodigoDetalle());
+		assertEquals(dr1.getCodigoDetalle().getDescripcion(), respuesta.getBody().getCodigoDetalle().getDescripcion());
+		logger.info("[RestAPI]Fin test: getDetalleRecibo");
+	}
+
+	@Test
+	void getAllDetalleRecibo() {
+		// codigoDetalle 1
+		CodigoDetalle cd1 = new CodigoDetalle();
+		cd1.setCodigoDetalle(100);
+		cd1.setDescripcion("La primer descripcion");
+
+		// codigoDetalle 2
+		CodigoDetalle cd2 = new CodigoDetalle();
+		cd2.setCodigoDetalle(200);
+		cd2.setDescripcion("La segunda descripcion");
+
+		// codigoDetalle 3
+		CodigoDetalle cd3 = new CodigoDetalle();
+		cd3.setCodigoDetalle(300);
+		cd3.setDescripcion("La tercer descripcion");
+
+		// ---------------------------------------------------
+
+		cd1 = codigoDetalleRepo.saveAndFlush(cd1);
+		cd2 = codigoDetalleRepo.saveAndFlush(cd2);
+		cd3 = codigoDetalleRepo.saveAndFlush(cd3);
+
+		// detalleRecibo 1
+		DetalleRecibo dr1 = new DetalleRecibo();
+		dr1.setCodigoDetalle(cd1);
+		dr1.setHaber(1500.00);
+		dr1.setDeduccion(null);
+		dr1.setPorcentaje(10.5);
+
+		// detalleRecibo 2
+		DetalleRecibo dr2 = new DetalleRecibo();
+		dr2.setCodigoDetalle(cd2);
+		dr2.setHaber(null);
+		dr2.setDeduccion(300.0);
+		dr2.setPorcentaje(15.0);
+
+		// detalleRecibo 2
+		DetalleRecibo dr3 = new DetalleRecibo();
+		dr3.setCodigoDetalle(cd3);
+		dr3.setHaber(400.0);
+		dr3.setDeduccion(null);
+		dr3.setPorcentaje(40.0);
+
+		// Guardamos en el repo
+
+		detalleRepo.saveAndFlush(dr1);
+		detalleRepo.saveAndFlush(dr2);
+		detalleRepo.saveAndFlush(dr3);
+
+		logger.info("[RestAPI]Inicio test: getAllDetalleRecibo");
+		String server = "http://localhost:" + puerto + ENDPOINT_DETALLE_SUELDO;
+
+		// Obetenemos los codigo detalle
+		ResponseEntity<DetalleRecibo[]> respuesta = testRestTemplate.getForEntity(server, DetalleRecibo[].class);
+		assertTrue(respuesta.getStatusCode().equals(HttpStatus.OK));
+		assertTrue(respuesta.getBody().length == 3);
+		logger.info("[RestAPI]Fin test: getAllDetalleRecibo");
+	}
+
+	@Test
+	void actualizarDetalleRecibo() {
+		logger.info("[RestAPI]Inicio test: actualizarDetalleRecibo");
+		String server = "http://localhost:" + puerto + ENDPOINT_DETALLE_SUELDO;
+		// codigoDetalle 1
+		CodigoDetalle cd1 = new CodigoDetalle();
+		cd1.setCodigoDetalle(100);
+		cd1.setDescripcion("La primer descripcion");
+
+		// codigoDetalle 2
+		CodigoDetalle cd2 = new CodigoDetalle();
+		cd2.setCodigoDetalle(200);
+		cd2.setDescripcion("La segunda descripcion");
+
+		// ---------------------------------------------------
+
+		cd1 = codigoDetalleRepo.saveAndFlush(cd1);
+		cd2 = codigoDetalleRepo.saveAndFlush(cd2);
+
+		// detalleRecibo 1
+		DetalleRecibo dr1 = new DetalleRecibo();
+		dr1.setCodigoDetalle(cd1);
+		dr1.setHaber(1500.00);
+		dr1.setDeduccion(null);
+		dr1.setPorcentaje(10.5);
+
+		detalleRepo.saveAndFlush(dr1);
+
+		// Re-configuramos la ruta del path
+		server += "/" + dr1.getId();
+
+		// Actualizamos parametros
+		
+		dr1.setCodigoDetalle(cd2);
+		dr1.setDeduccion(1000.0);
+		dr1.setHaber(null);
+		dr1.setPorcentaje(8.3);
+
+		
+		// La actualizamos
+		HttpEntity<DetalleRecibo> requestDetalleRecibo = new HttpEntity<>(dr1);
+		ResponseEntity<DetalleRecibo> respuesta = testRestTemplate.exchange(server, HttpMethod.PUT, 
+				requestDetalleRecibo,DetalleRecibo.class);
+		
+		assertTrue(respuesta.getStatusCode().equals(HttpStatus.OK));
+		assertEquals(dr1.getId(), respuesta.getBody().getId());
+		assertEquals(dr1.getDeduccion(), respuesta.getBody().getDeduccion());
+		assertEquals(dr1.getHaber(), respuesta.getBody().getHaber());
+		assertEquals(dr1.getPorcentaje(), respuesta.getBody().getPorcentaje());
+		assertEquals(dr1.getCodigoDetalle().getCodigoDetalle(),
+				respuesta.getBody().getCodigoDetalle().getCodigoDetalle());
+		assertEquals(dr1.getCodigoDetalle().getDescripcion(), respuesta.getBody().getCodigoDetalle().getDescripcion());
+	
+
+		// Chequeamos en el repo los cambios
+		Optional<DetalleRecibo> optCodigoDetalleActualizado = detalleRepo.findById(dr1.getId());
+		assertTrue(respuesta.getStatusCode().equals(HttpStatus.OK));
+		assertEquals(dr1.getId(), optCodigoDetalleActualizado.get().getId());
+		assertEquals(dr1.getDeduccion(), optCodigoDetalleActualizado.get().getDeduccion());
+		assertEquals(dr1.getHaber(), optCodigoDetalleActualizado.get().getHaber());
+		assertEquals(dr1.getPorcentaje(), optCodigoDetalleActualizado.get().getPorcentaje());
+		assertEquals(dr1.getCodigoDetalle().getCodigoDetalle(),
+				optCodigoDetalleActualizado.get().getCodigoDetalle().getCodigoDetalle());
+		assertEquals(dr1.getCodigoDetalle().getDescripcion(),
+				optCodigoDetalleActualizado.get().getCodigoDetalle().getDescripcion());
+		logger.info("[RestAPI]Fin test: actualizarDetalleRecibo");
 	}
 
 }
