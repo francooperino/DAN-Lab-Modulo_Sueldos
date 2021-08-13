8bf86f6f5dbb0eb52ab157c87c6e80c9780a7f1d
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-08-13 19:15:14 -0300

    Se agregaron ultimas configuraciones de docker

--- dockerfile
+++ dockerfile
@@ -1,8 +1,8 @@
 FROM openjdk:11.0.7-slim
 LABEL maintainer="nachobarzola97@gmail.com"
+VOLUME /tmp/reportes/recibos
 ARG JAR_FILE
 ADD target/${JAR_FILE} sueldos-0.0.1-SNAPSHOT.jar
 RUN echo ${JAR_FILE}
-EXPOSE 9001
+EXPOSE 9004
 ENTRYPOINT ["java","-jar","/sueldos-0.0.1-SNAPSHOT.jar"]
-
--- src/main/java/danms/sueldos/services/dao/EmpleadoRepository.java
+++ src/main/java/danms/sueldos/services/dao/EmpleadoRepository.java
@@ -2,10 +2,12 @@ package danms.sueldos.services.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import danms.sueldos.domain.Empleado;
 
 @Repository
+@Transactional(readOnly= true)
 public interface EmpleadoRepository extends JpaRepository<Empleado, Integer>{
 
 }
